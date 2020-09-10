b5ff23d07a4b5bba9dfc4b42b7c1d31139f356f7
DevashishPatwal <dpatwal8@outlook.com>
2020-09-10 18:52:38 +0530

    added page not found route

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require('express')
+const request = require('request')
+const url = 'https://programming-quotes-api.herokuapp.com/quotes/random'
+
 const port = process.env.PORT || 3000
 const app = express()
+
+app.get('', (req, res) => {
+    res.status(201).send('Home page')
+})
+
+app.get('*', (req, res) => {
+    res.status(404).send('Page Not Found')
+})
 
 app.listen(port, () => {
     console.log('Up and running at port', port)
