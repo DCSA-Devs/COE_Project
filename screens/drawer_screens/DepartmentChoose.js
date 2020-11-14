f3979021c082a1c947b135c24b5606448f278c9b
DevashishPatwal <dpatwal8@outlook.com>
2020-11-14 15:54:23 +0530

    Remove missing key warning

--- screens/drawer_screens/DepartmentChoose.js
+++ screens/drawer_screens/DepartmentChoose.js
@@ -1,135 +1,5 @@
-// import React, { Component } from "react";
-// import {
-//   Container,
-//   Header,
-//   Content,
-//   Left,
-//   Body,
-//   Icon,
-//   Text,
-//   ListItem,
-//   Item,
-//   Input,
-//   Right,
-// } from "native-base";
-// import { TouchableOpacity } from "react-native";
-// let helperArray = require("../../userList.json");
-// export default class DepartmentChoose extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       allUsers: helperArray,
-//       usersFiltered: helperArray,
-//     };
-//   }
-//   componentDidMount() {}
-//   searchUser = (text) => {
-//     this.setState({
-//       usersFiltered: this.state.allUsers.filter((i) =>
-//         i.name.toLowerCase().includes(text.toLowerCase())
-//       ),
-//     });
-//   };
-
-//   render() {
-//     return (
-//       <Container>
-//         <Header searchBar rounded>
-//           <Item>
-//             <Icon name="key" />
-//             <Input
-//               placeholder="Search"
-//               onChangeText={(text) => this.searchUser(text)}
-//             />
-//           </Item>
-//         </Header>
-//         <Content>
-//           {this.state.usersFiltered.map((item) => (
-//             <TouchableOpacity
-//               onPress={() => {
-//                 props.navigation.navigate("DepartmentScreen", item.name);
-//               }}
-//             >
-//               <ListItem avatar>
-//                 <Left>
-//                   <Image
-//                     source={require("../../assets/images/Ebooks.png")}
-//                     style={{ height: 20, width: 20 }}
-//                   />
-//                 </Left>
-//                 <Body>
-//                   <Text style={{ fontWeight: "bold" }}>{item.name}</Text>
-//                   <Text style={{ fontWeight: "300" }}>{item.class}</Text>
-//                   <Text style={{ fontWeight: "300" }}>{item.semester}</Text>
-//                 </Body>
-//                 <Right style={{ justifyContent: "center" }}>
-//                   <Icon name="arrow-forward" />
-//                 </Right>
-//               </ListItem>
-//             </TouchableOpacity>
-//           ))}
-//         </Content>
-//       </Container>
-//     );
-//   }
-// }
-
-// import React, { Component } from "react";
-// import { Avatar, TextInput } from "react-native-paper";
-// import { TouchableOpacity, View, Image } from "react-native";
-// let helperArray = require("../../userList.json");
-// export default class DepartmentChoose extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       allUsers: helperArray,
-//       usersFiltered: helperArray,
-//     };
-//   }
-//   componentDidMount() {}
-//   searchUser = (text) => {
-//     this.setState({
-//       usersFiltered: this.state.allUsers.filter((i) =>
-//         i.name.toLowerCase().includes(text.toLowerCase())
-//       ),
-//     });
-//   };
-
-//   render() {
-//     return (
-//       <View>
-//         <TextInput
-//           placeholder="Search"
-//           onChangeText={(text) => this.searchUser(text)}
-//         />
-
-//         {this.state.usersFiltered.map((item) => (
-//           <TouchableOpacity
-//             onPress={() => {
-//               props.navigation.navigate("DepartmentScreen", item.name);
-//             }}
-//           >
-//             <View style={{ flexDirection: "row" }}>
-//               <Image
-//                 source={require("../../assets/images/Ebooks.png")}
-//                 style={{ height: 20, width: 20 }}
-//               />
-//               <View>
-//                 <Text style={{ fontWeight: "bold" }}>{item.name}</Text>
-//                 <Text style={{ fontWeight: "300" }}>{item.class}</Text>
-//                 <Text style={{ fontWeight: "300" }}>{item.semester}</Text>
-//               </View>
-//               <Avatar.Icon name="arrow-forward" />
-//             </View>
-//           </TouchableOpacity>
-//         ))}
-//       </View>
-//     );
-//   }
-// }
-
 import React from "react";
-import { Avatar, Searchbar, Divider } from "react-native-paper";
+import { Searchbar, Divider } from "react-native-paper";
 import { TouchableOpacity, Text, View, Image } from "react-native";
 let helperArray = require("../../userList.json");
 import { Ionicons } from "@expo/vector-icons";
@@ -157,8 +27,8 @@ export default function DepartmentChoose({ navigation }) {
         placeholder="Search"
         onChangeText={(text) => searchUser(text)}
       />
-      {usersFiltered.map((item) => (
-        <>
+      {usersFiltered.map((item, index) => (
+        <View key={index}>
           <TouchableOpacity
             onPress={() => {
               navigation.navigate(item.navigate ? item.navigate : "");
@@ -190,7 +60,7 @@ export default function DepartmentChoose({ navigation }) {
             </View>
           </TouchableOpacity>
           <Divider />
-        </>
+        </View>
       ))}
     </View>
   );
